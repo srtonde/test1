d470c5f5caec6cf8d143064619f55185b06ff27d
Swapnil <Swapnil@DESKTOP-AOSNF3P>
2022-10-15 15:03:52 +0530

    first comit

---
+++ src/basic/test1.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class test1 
+{
+	public static void main (String[]args)
+	{
+		System.out.println("main method is running");
+		m1();	
+	}
+     
+	public static void m1()
+	{
+		System.out.println("hi");
+	}
+}
